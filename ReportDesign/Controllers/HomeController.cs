9f9755e7737ae4eef08d5b6cd81efd5a98706d3a
土豆泥拌辣椒 <12850950@qq.com>
2023-05-05 13:35:57 +0800

    project init

---
+++ ReportDesign/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetStudy.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    private DateTime _firstTime = DateTime.MinValue;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <summary>
+    /// 获取标记字段所有值
+    /// </summary>
+    [HttpPost]
+    [ActionName("getValue")]
+    public async Task<string> getValue()
+    {
+
+        return "succcess  " + _firstTime;
+    }
+
+}
+
